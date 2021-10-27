d63806f278ac2a27c91b9fdc61cb20f303b5ec04
Jonas Elysee <jonas.elysee@avanade.com>
2021-10-27 12:12:00 +0200

    Fixes build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserDetailsResolver } from '@usermanagement/user-details/resolvers/user-datails.resolver';
 import { UserDetailsComponent } from '@usermanagement/user-details/user-details.component';
-import { AuthorizationGuard } from './shared/guards/authorization.guard';
 import { UserListComponent } from './pages/user-management/user-list/user-list.component';
+import { AuthorizationGuard } from './shared/guards/authorization.guard';
 
 const routes: Routes = [
     { path: 'users', component: UserListComponent },
